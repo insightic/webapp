ce91b4489ff660987d45be37b38a954dc9e3075e
Yiwei Gong <imwithye@gmail.com>
2023-05-25 15:40:13 +0800

    better design

---
+++ src/components/Tag.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="m-1 tag">{{ tag }}</div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    tag: String
+  }
+}
+</script>
+
+<style scoped>
+.tag {
+    color: white;
+    background-color: rgba(39, 50, 131);
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    border-radius: 10px;
+}
+</style>
--- src/views/ProjectDetailsView.vue
+++ src/views/ProjectDetailsView.vue
@@ -19,11 +19,11 @@
         <li class="nav-item">
           <a
             class="nav-link active"
-            id="home-tab"
+            id="business-model-analysis-tab"
             data-bs-toggle="tab"
-            href="#home"
+            href="#business-model-analysis"
             role="tab"
-            aria-controls="home"
+            aria-controls="business-model-analysis"
             aria-selected="true"
             >Business Model Analysis</a
           >
@@ -66,8 +66,35 @@
         </li>
       </ul>
       <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-          ...
+        <div
+          class="tab-pane fade show active"
+          id="business-model-analysis"
+          role="tabpanel"
+          aria-labelledby="business-model-analysis-tab"
+        >
+          <div class="mt-3">
+            <div class="row">
+              <div class="col-md-7">
+                <h3>Business model & Value Proposition</h3>
+                <p>
+                  Biswap is a trusted DEX platform on the BNB Chain Network with a Multi-type
+                  Referral Program and low trade fee starting from 0.1%. Biswap is the ecosystem
+                  that offers the best service and creates new standards in DeFi.
+                </p>
+                <h3>Customer Segments</h3>
+                <p>
+                  Retail investors who need to swap tokens Institutiona funds who conduct AMM
+                  business VASPs who IDO.
+                </p>
+              </div>
+              <div class="col-md-5">
+                <div class="d-flex flex-wrap">
+                  <Tag tag="P2P Trading" />
+                  <Tag tag="Deposits" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
           ...
@@ -81,7 +108,12 @@
 </template>
 
 <script lang="ts">
+import Tag from '@/components/Tag.vue'
+
 export default {
+  components: {
+    Tag
+  },
   data() {
     return {
       author: 'Yiwei Gong',
@@ -98,10 +130,10 @@ export default {
 
 <style scoped>
 .nav-link {
-    color: rgb(135, 136, 141);
+  color: rgb(135, 136, 141);
 }
 
 .nav-link.active {
-    color: rgba(39, 50, 131) !important;
+  color: rgba(39, 50, 131) !important;
 }
 </style>
