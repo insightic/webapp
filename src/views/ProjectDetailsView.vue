de3f629090887c6906243ac26e2b832ca016bd38
Yiwei Gong <imwithye@gmail.com>
2023-05-25 15:59:37 +0800

    update data

--- src/views/ProjectDetailsView.vue
+++ src/views/ProjectDetailsView.vue
@@ -31,11 +31,11 @@
         <li class="nav-item">
           <a
             class="nav-link"
-            id="profile-tab"
+            id="social-media-analysis-tab"
             data-bs-toggle="tab"
-            href="#profile"
+            href="#social-media-analysis"
             role="tab"
-            aria-controls="profile"
+            aria-controls="social-media-analysis"
             aria-selected="false"
             >Social Media Analysis</a
           >
@@ -43,11 +43,11 @@
         <li class="nav-item">
           <a
             class="nav-link"
-            id="contact-tab"
+            id="on-chain-data-analysis-tab"
             data-bs-toggle="tab"
-            href="#contact"
+            href="#on-chain-data-analysis"
             role="tab"
-            aria-controls="contact"
+            aria-controls="on-chain-data-analysis"
             aria-selected="false"
             >On-Chain Data Analysis</a
           >
@@ -55,11 +55,11 @@
         <li class="nav-item">
           <a
             class="nav-link"
-            id="contact-tab"
+            id="team-background-analysis-tab"
             data-bs-toggle="tab"
-            href="#contact"
+            href="#team-background-analysis"
             role="tab"
-            aria-controls="contact"
+            aria-controls="team-background-analysis"
             aria-selected="false"
             >Team Background Analysis</a
           >
@@ -102,11 +102,118 @@
             </div>
           </div>
         </div>
-        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-          ...
+        <div
+          class="tab-pane fade"
+          id="social-media-analysis"
+          role="tabpanel"
+          aria-labelledby="social-media-analysis-tab"
+        >
+          <div class="mt-3">
+            <div class="row">
+              <div class="col-md-7">
+                <h3>Twitter Analysis</h3>
+                <p>
+                  Biswap is a trusted DEX platform on the BNB Chain Network with a Multi-type
+                  Referral Program and low trade fee starting from 0.1%. Biswap is the ecosystem
+                  that offers the best service and creates new standards in DeFi.
+                </p>
+                <h3>Blog Analysis</h3>
+                <p>
+                  Retail investors who need to swap tokens Institutiona funds who conduct AMM
+                  business VASPs who IDO.
+                </p>
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <TextCard class="col-5 m-1" label="Revenue" text="$405,091.00" />
+                  <TextCard class="col-5 m-1" label="Overdue invoices" text="$12,787.00" />
+                  <TextCard class="col-5 m-1" label="Outstanding invoices" text="$245,988.00" />
+                  <TextCard class="col-5 m-1" label="Expenses" text="$30,156.00" />
+                </div>
+                <div class="d-flex flex-wrap mt-2">
+                  <Tag tag="P2P Trading" />
+                  <Tag tag="Deposits" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-          ...
+        <div
+          class="tab-pane fade"
+          id="on-chain-data-analysis"
+          role="tabpanel"
+          aria-labelledby="on-chain-data-analysis-tab"
+        >
+          <div class="mt-3">
+            <div class="row">
+              <div class="col-md-7">
+                <h3>Business model & Value Proposition</h3>
+                <p>
+                  Biswap is a trusted DEX platform on the BNB Chain Network with a Multi-type
+                  Referral Program and low trade fee starting from 0.1%. Biswap is the ecosystem
+                  that offers the best service and creates new standards in DeFi.
+                </p>
+                <h3>Customer Segments</h3>
+                <p>
+                  Retail investors who need to swap tokens Institutiona funds who conduct AMM
+                  business VASPs who IDO.
+                </p>
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <TextCard class="col-5 m-1" label="Revenue" text="$405,091.00" />
+                  <TextCard class="col-5 m-1" label="Overdue invoices" text="$12,787.00" />
+                  <TextCard class="col-5 m-1" label="Outstanding invoices" text="$245,988.00" />
+                  <TextCard class="col-5 m-1" label="Expenses" text="$30,156.00" />
+                </div>
+                <div class="d-flex flex-wrap mt-2">
+                  <Tag tag="P2P Trading" />
+                  <Tag tag="Deposits" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="tab-pane fade"
+          id="team-background-analysis"
+          role="tabpanel"
+          aria-labelledby="team-background-analysis-tab"
+        >
+          <div class="mt-3">
+            <div class="row">
+              <div class="col-md-7">
+                <h3>On-Chain Analysis</h3>
+                <p>
+                  Biswap is a trusted DEX platform on the BNB Chain Network with a Multi-type
+                  Referral Program and low trade fee starting from 0.1%. Biswap is the ecosystem
+                  that offers the best service and creates new standards in DeFi.
+                </p>
+                <h3>Twitter Analysis</h3>
+                <p>
+                  Retail investors who need to swap tokens Institutiona funds who conduct AMM
+                  business VASPs who IDO.
+                </p>
+                <h3>LinkedIn Analysis</h3>
+                <p>
+                  Retail investors who need to swap tokens Institutiona funds who conduct AMM
+                  business VASPs who IDO.
+                </p>
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <TextCard class="col-5 m-1" label="Revenue" text="$405,091.00" />
+                  <TextCard class="col-5 m-1" label="Overdue invoices" text="$12,787.00" />
+                  <TextCard class="col-5 m-1" label="Outstanding invoices" text="$245,988.00" />
+                  <TextCard class="col-5 m-1" label="Expenses" text="$30,156.00" />
+                </div>
+                <div class="d-flex flex-wrap mt-2">
+                  <Tag tag="P2P Trading" />
+                  <Tag tag="Deposits" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
