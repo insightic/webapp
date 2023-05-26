cd415daf1d0a725f77d58c51d289659829c25edd
Yiwei Gong <imwithye@gmail.com>
2023-05-26 17:57:02 +0800

    add tags

--- src/components/AttrDisplayCard.vue
+++ src/components/AttrDisplayCard.vue
@@ -5,14 +5,23 @@
       <p class="card-text">
         {{ prop?.text }}
       </p>
+      <div v-if="prop?.tags">
+        <b>Tags:</b>
+        <div class="d-flex"><Tag v-for="(tag, idx) in prop?.tags" :key="idx" :tag="tag" /></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import Tag from '@/components/Tag.vue'
+
 export default {
   props: {
     prop: Object
+  },
+  components: {
+    Tag
   }
 }
 </script>
