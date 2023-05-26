06a422bdbd6ba77238ab36e76c671e46ffe07af9
Yiwei Gong <imwithye@gmail.com>
2023-05-26 16:14:03 +0800

    fix subtabs

--- src/components/ProjectSidebarItem.vue
+++ src/components/ProjectSidebarItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex p-2 project-sidebar-item">
-    <div style="margin-right: 20px"><img :src="logo" class="logo"/></div>
-    <div class="text" :class="{highlighted: highlighted}">{{ name }}</div>
+    <div style="margin-right: 20px"><img :src="logo" class="logo" /></div>
+    <div class="text" :class="{ highlighted: highlighted }">{{ name }}</div>
   </div>
 </template>
 
@@ -18,28 +18,28 @@ export default {
 
 <style scoped>
 .project-sidebar-item {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .project-sidebar-item:hover {
-    color: rgba(39, 50, 131);
+  color: rgba(39, 50, 131);
 }
 
 .logo {
-    width: 32px;
-    height: 32px;
-    border-radius: 50%;
-    border: 1px solid rgba(39, 50, 131);
-    background-color: white;
-    padding: 3px;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 1px solid rgba(39, 50, 131);
+  background-color: white;
+  padding: 3px;
 }
 
 .text {
-    line-height: 32px;
+  line-height: 32px;
 }
 
 .highlighted {
-    color: rgba(39, 50, 131);
-    font-weight: bold;
+  color: rgba(39, 50, 131);
+  font-weight: bold;
 }
 </style>
--- src/views/ProjectDetailsView.vue
+++ src/views/ProjectDetailsView.vue
@@ -31,49 +31,25 @@
         <li class="nav-item">
           <a
             class="nav-link"
-            id="business-model-analysis-tab"
+            id="assessment-tab"
             data-bs-toggle="tab"
-            href="#business-model-analysis"
+            href="#assessment"
             role="tab"
-            aria-controls="business-model-analysis"
+            aria-controls="assessment"
             aria-selected="true"
-            >Business Model Analysis</a
+            >Assessment</a
           >
         </li>
         <li class="nav-item">
           <a
             class="nav-link"
-            id="social-media-analysis-tab"
+            id="actions-tab"
             data-bs-toggle="tab"
-            href="#social-media-analysis"
+            href="#actions"
             role="tab"
-            aria-controls="social-media-analysis"
+            aria-controls="actions"
             aria-selected="false"
-            >Social Media Analysis</a
-          >
-        </li>
-        <li class="nav-item">
-          <a
-            class="nav-link"
-            id="on-chain-data-analysis-tab"
-            data-bs-toggle="tab"
-            href="#on-chain-data-analysis"
-            role="tab"
-            aria-controls="on-chain-data-analysis"
-            aria-selected="false"
-            >On-Chain Data Analysis</a
-          >
-        </li>
-        <li class="nav-item">
-          <a
-            class="nav-link"
-            id="team-background-analysis-tab"
-            data-bs-toggle="tab"
-            href="#team-background-analysis"
-            role="tab"
-            aria-controls="team-background-analysis"
-            aria-selected="false"
-            >Team Background Analysis</a
+            >Actions</a
           >
         </li>
       </ul>
@@ -134,18 +110,13 @@
         </div>
         <div
           class="tab-pane fade show"
-          id="business-model-analysis"
+          id="assessment"
           role="tabpanel"
-          aria-labelledby="business-model-analysis-tab"
+          aria-labelledby="assessment-tab"
         >
           <div class="mt-3"></div>
         </div>
-        <div
-          class="tab-pane fade"
-          id="social-media-analysis"
-          role="tabpanel"
-          aria-labelledby="social-media-analysis-tab"
-        >
+        <div class="tab-pane fade" id="actions" role="tabpanel" aria-labelledby="actions-tab">
           <div class="mt-3">
             <div class="row">
               <div class="col-md-7">
@@ -156,83 +127,6 @@
                   that offers the best service and creates new standards in DeFi.
                 </p>
                 <h3>Blog Analysis</h3>
-                <p>
-                  Retail investors who need to swap tokens Institutiona funds who conduct AMM
-                  business VASPs who IDO.
-                </p>
-              </div>
-              <div class="col-md-5">
-                <div class="row">
-                  <TextCard class="col-5 m-1" label="Revenue" text="$405,091.00" />
-                  <TextCard class="col-5 m-1" label="Overdue invoices" text="$12,787.00" />
-                  <TextCard class="col-5 m-1" label="Outstanding invoices" text="$245,988.00" />
-                  <TextCard class="col-5 m-1" label="Expenses" text="$30,156.00" />
-                </div>
-                <div class="d-flex flex-wrap mt-2">
-                  <Tag tag="P2P Trading" />
-                  <Tag tag="Deposits" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          class="tab-pane fade"
-          id="on-chain-data-analysis"
-          role="tabpanel"
-          aria-labelledby="on-chain-data-analysis-tab"
-        >
-          <div class="mt-3">
-            <div class="row">
-              <div class="col-md-7">
-                <h3>Business model & Value Proposition</h3>
-                <p>
-                  Biswap is a trusted DEX platform on the BNB Chain Network with a Multi-type
-                  Referral Program and low trade fee starting from 0.1%. Biswap is the ecosystem
-                  that offers the best service and creates new standards in DeFi.
-                </p>
-                <h3>Customer Segments</h3>
-                <p>
-                  Retail investors who need to swap tokens Institutiona funds who conduct AMM
-                  business VASPs who IDO.
-                </p>
-              </div>
-              <div class="col-md-5">
-                <div class="row">
-                  <TextCard class="col-5 m-1" label="Revenue" text="$405,091.00" />
-                  <TextCard class="col-5 m-1" label="Overdue invoices" text="$12,787.00" />
-                  <TextCard class="col-5 m-1" label="Outstanding invoices" text="$245,988.00" />
-                  <TextCard class="col-5 m-1" label="Expenses" text="$30,156.00" />
-                </div>
-                <div class="d-flex flex-wrap mt-2">
-                  <Tag tag="P2P Trading" />
-                  <Tag tag="Deposits" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          class="tab-pane fade"
-          id="team-background-analysis"
-          role="tabpanel"
-          aria-labelledby="team-background-analysis-tab"
-        >
-          <div class="mt-3">
-            <div class="row">
-              <div class="col-md-7">
-                <h3>On-Chain Analysis</h3>
-                <p>
-                  Biswap is a trusted DEX platform on the BNB Chain Network with a Multi-type
-                  Referral Program and low trade fee starting from 0.1%. Biswap is the ecosystem
-                  that offers the best service and creates new standards in DeFi.
-                </p>
-                <h3>Twitter Analysis</h3>
-                <p>
-                  Retail investors who need to swap tokens Institutiona funds who conduct AMM
-                  business VASPs who IDO.
-                </p>
-                <h3>LinkedIn Analysis</h3>
                 <p>
                   Retail investors who need to swap tokens Institutiona funds who conduct AMM
                   business VASPs who IDO.
