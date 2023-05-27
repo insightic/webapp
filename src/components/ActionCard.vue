3093b697a30ef8682ea4d1df5a26ae7bde19403a
Yiwei Gong <imwithye@gmail.com>
2023-05-27 17:01:22 +0800

    add action card

---
+++ src/components/ActionCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="action-card p-3 py-4">
+    <div class="d-flex">
+      <div
+        class="align-self-center"
+        style="
+          margin-right: 10px;
+          color: white;
+          border-radius: 4px;
+          background-color: rgba(39, 50, 131);
+          width: 64px;
+          height: 64px;
+          font-size: 1.5rem;
+          text-align: center;
+          line-height: 64px;
+        "
+      >
+        <i class="bi bi-bell-fill"></i>
+      </div>
+      <div class="align-self-center" style="text-align: center; flex-grow: 1;">
+        <div style="font-size: 0.7rem; color: rgb(135, 136, 141);">{{ title }}</div>
+        <div style="font-size: 2rem;">{{ status }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['title', 'status', 'changes']
+}
+</script>
+
+<style scoped>
+.action-card {
+  border-radius: 4px;
+  border: 1px solid rgba(39, 50, 131);
+}
+</style>
--- src/views/ProjectActionView.vue
+++ src/views/ProjectActionView.vue
@@ -1,3 +1,21 @@
 <template>
-    <div>Hello</div>
+  <div>
+    <div class="row my-3">
+      <div class="col-md-4"><ActionCard title="Total Suggestion" status="17" /></div>
+      <div class="col-md-4"><ActionCard title="Completed" status="5" /></div>
+      <div class="col-md-4"><ActionCard title="Action Needed" status="8" /></div>
+    </div>
+    <h3>Action</h3>
+  </div>
 </template>
+
+<script lang="ts">
+import ActionCard from '@/components/ActionCard.vue'
+
+export default {
+  props: ['project'],
+  components: {
+    ActionCard
+  }
+}
+</script>
