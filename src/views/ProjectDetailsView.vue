febb6a66d96fad0e744057f3429faa4d64eb7474
Yiwei Gong <imwithye@gmail.com>
2023-05-27 09:11:11 +0800

    add assessment view

--- src/views/ProjectDetailsView.vue
+++ src/views/ProjectDetailsView.vue
@@ -78,10 +78,8 @@
           role="tabpanel"
           aria-labelledby="overview-tab"
         >
-          <div class="mt-3 row">
-            <div class="col-md-6 p-2" v-for="(prop, idx) in project?.props" :key="idx">
-              <PropDisplayGroup :title="prop.title" :props="prop.props"/>
-            </div>
+          <div class="mt-3">
+            <ProjectOverviewView :project="project" />
           </div>
         </div>
         <div
@@ -90,37 +88,8 @@
           role="tabpanel"
           aria-labelledby="assessment-tab"
         >
-          <div class="mt-3"></div>
-        </div>
-        <div class="tab-pane fade" id="actions" role="tabpanel" aria-labelledby="actions-tab">
           <div class="mt-3">
-            <div class="row">
-              <div class="col-md-7">
-                <h3>Twitter Analysis</h3>
-                <p>
-                  Biswap is a trusted DEX platform on the BNB Chain Network with a Multi-type
-                  Referral Program and low trade fee starting from 0.1%. Biswap is the ecosystem
-                  that offers the best service and creates new standards in DeFi.
-                </p>
-                <h3>Blog Analysis</h3>
-                <p>
-                  Retail investors who need to swap tokens Institutiona funds who conduct AMM
-                  business VASPs who IDO.
-                </p>
-              </div>
-              <div class="col-md-5">
-                <div class="row">
-                  <TextCard class="col-5 m-1" label="Revenue" text="$405,091.00" />
-                  <TextCard class="col-5 m-1" label="Overdue invoices" text="$12,787.00" />
-                  <TextCard class="col-5 m-1" label="Outstanding invoices" text="$245,988.00" />
-                  <TextCard class="col-5 m-1" label="Expenses" text="$30,156.00" />
-                </div>
-                <div class="d-flex flex-wrap mt-2">
-                  <Tag tag="P2P Trading" />
-                  <Tag tag="Deposits" />
-                </div>
-              </div>
-            </div>
+            <ProjectAssessmentView :project="project" />
           </div>
         </div>
       </div>
@@ -130,8 +99,9 @@
 
 <script lang="ts">
 import Tag from '@/components/Tag.vue'
-import TextCard from '@/components/TextCard.vue'
-import PropDisplayGroup from '@/components/PropDisplayGroup.vue'
+// import TextCard from '@/components/TextCard.vue'
+import ProjectOverviewView from './ProjectOverviewView.vue'
+import ProjectAssessmentView from './ProjectAssessmentView.vue'
 import { organizationsStore } from '@/stores/organizations'
 import { mapStores } from 'pinia'
 
@@ -142,8 +112,8 @@ export default {
   },
   components: {
     Tag,
-    TextCard,
-    PropDisplayGroup
+    ProjectOverviewView,
+    ProjectAssessmentView
   },
   data() {
     return {
---
+++ src/views/ProjectOverviewView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <div class="col-md-6 p-2" v-for="(prop, idx) in project?.props" :key="idx">
+      <PropDisplayGroup :title="prop.title" :props="prop.props" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import PropDisplayGroup from '@/components/PropDisplayGroup.vue'
+
+export default {
+  props: ['project'],
+  components: {
+    PropDisplayGroup
+  }
+}
+</script>
