a1a01f7b9a2e5a8ac5de8938311306cee853cece
Yiwei Gong <imwithye@gmail.com>
2023-05-29 23:10:13 +0800

    add pool together

--- src/stores/organizations.ts
+++ src/stores/organizations.ts
@@ -2,6 +2,7 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import BiSwap_LP from './sample_data/BiSwap_LP'
 import Wolf_Game from './sample_data/Wolf_Game'
+import Pool_Together from './sample_data/Pool_Together'
 import EulerFinance from './sample_data/Euler_Finance'
 
 export const organizationsStore = defineStore('organizations', () => {
@@ -12,18 +13,7 @@ export const organizationsStore = defineStore('organizations', () => {
             projects: [
                 BiSwap_LP,
                 Wolf_Game,
-                {
-                    guid: "e262d5c2-16f8-47a0-8c70-4019514d137c",
-                    logo: "/webapp/logos/pooltogether.jpeg",
-                    name: "Pool Together",
-                    url: "https://pooltogether.com",
-                    author: "John Doe",
-                    rating: "B-",
-                    description: "Pool Together is a lottery game",
-                    tags: ["Game", "NFT"],
-                    status: "In Progress",
-                    dueAt: "2023-05-25T00:00:00.000Z",
-                },
+                Pool_Together,
                 {
                     guid: "e262d5c2-16f8-47a0-8c70-4019514d137d",
                     logo: "/webapp/logos/olympus.jpeg",
