42371a43d6dae61456147171d84f2e9344e750a4
Yiwei Gong <imwithye@gmail.com>
2023-05-30 09:09:13 +0800

    add data

--- src/stores/sample_data/Euler_Finance.ts
+++ src/stores/sample_data/Euler_Finance.ts
@@ -145,5 +145,61 @@ export default {
                 }
             ]
         }
-    ]
+    ],
+    actionsView: {
+        actions: [
+            {
+                title: 'Require 1 or more security audit report',
+                text: 'Suggest the VASP to submit at least another security audit report within 45 days...',
+                type: 'danger'
+            },
+            {
+                title: 'Code has not been updates for long time',
+                text: 'Suggest the VASP to link Github account or submit a new code base.',
+                type: 'warn'
+            },
+            {
+                title: 'Risky wallets interaction',
+                text: 'The deployed contract has attracted many newly wallets. Require VASP to validate...',
+                type: 'primary'
+            },
+            {
+                title: 'Code has high similarity to Uniswap',
+                text: 'Suggest the VASP to further clarify the differences for not violating Business S...',
+                type: 'danger'
+            },
+            {
+                title: 'High botting rate',
+                text: "Suggest to review the VASP's solution on bot detection and IP restriction.",
+                type: 'warn'
+            },
+            {
+                title: 'Core contract has been modified recently',
+                text: 'Dangerous to update the core smart contract code. The audit reports may not be s...',
+                type: 'primary'
+            },
+            {
+                title: 'Orderbook Spoofing',
+                text: 'There is a high chance that the order book design has no restriction on spoofing...',
+                type: 'danger'
+            }
+        ],
+        tasks: [
+            {
+                title: 'Require 1 or more security audit report',
+                text: 'Suggest the VASP to submit at least another security audit report within 45 days...',
+                type: 'danger'
+            },
+            {
+                title: 'Code has not been updates for long time',
+                text: 'Suggest the VASP to link Github account or submit a new code base.',
+                type: 'warn'
+            },
+            {
+                title: 'Risky wallets interaction',
+                text: 'The deployed contract has attracted many newly wallets. Require VASP to validate...',
+                type: 'primary'
+            }
+        ]
+    }
 }
