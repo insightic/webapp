efcb67cf27599f37c397455eaaf5cce3e4af709c
Yiwei Gong <imwithye@gmail.com>
2023-06-22 08:30:03 +0800

    add kyc page

---
+++ src/views/projectSubViews/ProjectKYCView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="text-secondary mb-4">Know Your Customer Verification</div>
+
+  <div style="max-width: 960px">
+    <SectionLayout title="Corporate Documents">
+      <div class="row">
+        <div class="col-md-6">
+          <LabelInputComponent label="Certificate of incorporation" type="file" />
+        </div>
+        <div class="col-md-6">
+          <LabelInputComponent label="Constitution or Memorandum of association" type="file" />
+        </div>
+        <div class="col-md-6">
+          <LabelInputComponent label="Directors" type="text" />
+        </div>
+        <div class="col-md-6">
+          <LabelInputComponent label="Members" type="text" />
+        </div>
+      </div>
+    </SectionLayout>
+    <SectionLayout title="UOB (Ultimate Beneficial Owner) Information">
+      <div class="row">
+        <div class="col-md-6">
+          <LabelInputComponent label="Name" type="text" />
+        </div>
+        <div class="col-md-6">
+          <LabelInputComponent label="Nationality" type="text" />
+        </div>
+      </div>
+      <LabelInputComponent label="Address 1" type="text" />
+      <LabelInputComponent label="Address 2" type="text" />
+      <div class="row">
+        <div class="col-md-6">
+          <LabelInputComponent label="Passport" type="file" />
+        </div>
+        <div class="col-md-6">
+          <LabelInputComponent label="Proof of Residency" type="file" />
+        </div>
+      </div>
+    </SectionLayout>
+    <SectionLayout title="Organization Chart">
+        <LabelInputComponent label="Please follow the screenshot below to clearly illustrate the share distribution" type="file" />
+        <img src="/org-chart.png" class="my-2" style="max-width: 320px;"/>
+    </SectionLayout>
+
+    <button type="button" class="btn btn-primary">Save and Submit</button>
+  </div>
+</template>
+
+<script lang="ts">
+import SectionLayout from '@/layouts/SectionLayout.vue'
+import LabelInputComponent from '@/components/LabelInputComponent.vue'
+
+export default {
+  components: {
+    SectionLayout,
+    LabelInputComponent
+  }
+}
+</script>
