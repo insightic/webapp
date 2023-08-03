67f40554ece85b4d04254a39040936e820811d2c
unknown <e0407783@u.nus.edu>
2023-08-03 23:16:06 +0800

    divided form into sections and implemented some basic validation

--- src/views/accountSubViews/AccountCreateProjectView_backup.vue
+++ src/views/accountSubViews/AccountCreateProjectView_backup.vue
@@ -1,47 +1,21 @@
 <template>
-  <div class="createProject mx-0 py-3"> 
-    <!-- <h4 class="mt-3 mb-2">Create a New Project</h4> -->
-
-    <div class="d-flex align-items-start mb-3">
-      <h4 class="me-2" @click="current=1" :class="current==1 ? 'active-section' : ''">
-        <i v-if="current==1" class="bi bi-1-square-fill"></i>
-        <i v-else class="bi bi-1-square"></i>
-        <span className="ms-2">Company</span>
-      </h4>
-      <span><i class="bi bi-chevron-right"></i></span>
-      <h4 class="mx-2" @click="current=2" :class="current==2 ? 'active-section' : ''">
-        <i v-if="current==2" class="bi bi-2-square-fill"></i>
-        <i v-else class="bi bi-2-square"></i>
-        <span className="ms-2">Team</span>
-      </h4>
-      <span><i class="bi bi-chevron-right"></i></span>
-      <h4 class="mx-2" @click="current=3" :class="current==3 ? 'active-section' : ''">
-        <i v-if="current==3" class="bi bi-3-square-fill"></i>
-        <i v-else class="bi bi-3-square"></i>
-        <span className="ms-2">Objective</span>
-      </h4>
-      <span><i class="bi bi-chevron-right"></i></span>
-      <h4 class="mx-2" @click="current=4" :class="current==4 ? 'active-section' : ''">
-        <i v-if="current==4" class="bi bi-4-square-fill"></i>
-        <i v-else class="bi bi-4-square"></i>
-        <span className="ms-2">Purpose</span>
-      </h4>
-      <span><i class="bi bi-chevron-right"></i></span>
-      <h4 class="ms-2" @click="current=5" :class="current==5 ? 'active-section' : ''">
-        <i v-if="current==5" class="bi bi-5-square-fill"></i>
-        <i v-else class="bi bi-5-square"></i>
-        <span className="ms-2">Assets</span>
-      </h4>
-    </div>
+  <div class="createProject mx-3 p-3">
+    <h4 class="mt-3 mb-2">Create a New Project</h4>
     <div class="d-grid form rounded border-1 p-3">
-      <section class="border-1 mb-3">
+      <section class="border-1 mb-5">
+        <div
+          class="mt-1 mb-3 py-2 px-3 secHeader d-flex align-items-center"
+          @click="show1 = !show1"
+        >
+          <i class="bi bi-caret-down-square-fill pe-2"></i>
+          <h5 class="mb-0 me-2">Part A: DLT Foundation Details</h5>
+          <span v-show="complete1 == true" class="text-success">✓</span>
+          <span v-show="complete1 != true" class="text-danger">*</span>
+        </div>
+
         <Transition name="smooth" mode="out-in">
-          <div v-if="current==1" class="form-content ms-3 d-flex flex-column">
-            <div class="mt-1 mb-3 py-2 d-flex align-items-center">
-              <h5 class="mb-0 me-2">Part A: DLT Foundation Details</h5>
-              <span v-show="complete1 == true" class="text-success">✓</span>
-              <span v-show="complete1 != true" class="text-danger">*</span>
-            </div>
+
+          <div v-if="show1" class="form-content ms-3">
             <LabelInputComponent label="DLT Foundation Name" type="text" v-model:field="name" :required="true"/>
             <LabelInputComponent label="Official Twitter Account (URL)" type="text" v-model:field="twitter" :required="true"/>
             <LabelInputComponent label="Official Website (URL)" type="text" v-model:field="website" :required="true"/>
@@ -60,17 +34,25 @@
                 Please attach a quality version of the whitepaper document.
               </div>
             </div>
-            <div class="nav-btn mt-5 d-flex align-items-center justify-content-center">
-              <button type="button" class="btn btn-primary" @click="nextStep(1)">Next</button>
-            </div>
-
+          
           </div>
+        </Transition>
+      </section>
 
-          <div v-else-if="current==2" class="form-content ms-3">
-            <div class="mt-1 mb-3 py-2 d-flex align-items-center">
-              <h5 class="mb-0 me-2">Part B: Founder(s) Details</h5>
-            </div>
+      <section class="border-1 mb-5">
+        <div
+          class="mt-1 mb-3 py-2 px-3 secHeader d-flex align-items-center"
+          @click="show2 = !show2"
+        >
+          <i class="bi bi-caret-down-square-fill pe-2"></i>
+          <h5 class="mb-0 me-2">Part B: Founder(s) Details</h5>
+          <span v-show="complete2 == true" class="text-success">✓</span>
+          <span v-show="complete2 != true" class="text-danger">*</span>
+        </div>
 
+        <Transition name="smooth" mode="out-in">
+          <!-- <div v-if="show2" class="form-content ms-3" :style="{maxHeight: calcHeight }"> -->
+          <div v-if="show2" class="form-content2 ms-3">
             <div class="">
               <label class="form-label" for="teamMembers">
                 Founders
@@ -131,7 +113,6 @@
                 </div>
               </div>
               
-              
               <div class="mt-5">
                 <label class="form-label" for="teamMembers">
                   Team Members
@@ -149,83 +130,157 @@
                 </div>
               </div>
               
-            </div>
-            <div class="nav-btn mt-5 d-flex align-items-center justify-content-center">
-              <button type="button" class="btn btn-outline-primary me-3" @click="current=current-1">Back</button>
-              <button type="button" class="btn btn-primary" @click="nextStep(2)">Next</button>
-            </div>
-            
+              <!-- <div class="">
+                <div class="mb-2" v-for="(founder, counter) in founders" v-bind:key="counter">
+                  <div class="d-flex">
+                    <div class="fw-bold">Founder {{ counter + 1 }}</div>
+                    <span class="delete-btn text-danger ms-3" @click="deleteFounder(counter)">Remove founder</span>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <LabelInputComponent label="Name" type="text" v-model:field="founder.name" :required="true"/>
+                    </div>
+                    <div class="col-md-6">
+                      <LabelInputComponent label="Position within DLT Foundation" type="text" v-model:field="founder.position" :required="true"/>
+                    </div>
+                  </div>
 
-          </div>
+                  <LabelInputComponent label="KYC Verification" type="text" v-model:field="founder.kyc" :required="true"/>
 
-          <div v-else-if="current==3" class="form-content ms-3">
-            <div class="mt-1 mb-3 py-2 d-flex align-items-center">
-              <h5 class="mb-0 me-2">Part C: Objectives of the DLT Foundation</h5>
-              <span v-show="complete3 == true" class="text-success">✓</span>
-              <span v-show="complete3 != true" class="text-danger">*</span>
-            </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <LabelInputComponent label="Twitter Account (URL)" type="text" v-model:field="founder.twitter" :required="true"/>
+                    </div>
+                    <div class="col-md-6">
+                      <LabelInputComponent label="LinkedIn Profile (URL)" type="text" v-model:field="founder.linkedin" :required="true"/>
+                    </div>
+                  </div>
 
-            <div class="form-content">
-              <LabelTextareaComponent label="Objectives of your DLT Foundation" 
-                footnote="Please provide a detailed description of the objectives of your DLT Foundation (max. 5000 characters)" 
-                type="text" v-model:field="objective" :required="true"/>
-            </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <LabelInputComponent label="Email Address" type="text" v-model:field="founder.email" :required="true"/>
+                    </div>
+                    <div class="col-md-6">
+                      <LabelInputComponent label="Ethereum Address" type="text" v-model:field="founder.ethereum" :required="true"/>
+                    </div>
+                  </div>
 
-            <div class="nav-btn mt-5 d-flex align-items-center justify-content-center">
-              <button type="button" class="btn btn-outline-primary me-3" @click="current=current-1">Back</button>
-              <button type="button" class="btn btn-primary" @click="nextStep(3)">Next</button>
-            </div>
+                  <div class="">
+                    <label for="document" class=""
+                      >CV</label
+                    >
+                    <input
+                      type="file"
+                      class="form-controls w-100"
+                      id="document"
+                    />
+                    <div class="text-secondary small">
+                      Brief CV or Biography (Please attach separate sheets if necessary)
+                    </div>
+                  </div>
 
-          </div>
 
-          <div v-else-if="current==4" class="form-content ms-3">
-            <div class="mt-1 mb-3 py-2 d-flex align-items-center">
-              <h5 class="mb-0 me-2">Part D: Specific Purpose</h5>
-              <span v-show="complete4 == true" class="text-success">✓</span>
-              <span v-show="complete4 != true" class="text-danger">*</span>
-            </div>
-            <div class="form-content"> 
-              <LabelTextareaComponent label="Motivation of your DLT Foundation" 
-                footnote="If your DLT Foundation has been established for a specific purpose, please detail the purpose (max. 5000 characters)" 
-                type="text" v-model:field="motivation" :required="true"/>
-            </div>
-
-            <div class="nav-btn mt-5 d-flex align-items-center justify-content-center">
-              <button type="button" class="btn btn-outline-primary me-3" @click="current=current-1">Back</button>
-              <button type="button" class="btn btn-primary" @click="nextStep(4)">Next</button>
-            </div>
-          </div>
-
-          <div v-else-if="current==5" class="form-content ms-3">
-            <div class="mt-1 mb-3 py-2 d-flex align-items-center">
-              <h5 class="mb-0 me-2">Part E: Initial Assets</h5>
-              <span v-show="complete5 == true" class="text-success">✓</span>
-          <span v-show="complete5 != true" class="text-danger">*</span>
-            </div>
-            <div class="form-content">
-              <LabelTextareaComponent label="Initial Assets of your DLT Foundation" 
-                footnote="Please provide a detailed description of the initial assets of your DLT Foundation (max. 5000 characters):" 
-                type="text" v-model:field="assets" :required="true"/>
+                </div>
+                <div class="w-100 d-flex justify-content-center">
+                  <i class="bi bi-plus-circle add-btn" @click="addFounder"></i>
+                </div>
+              </div>
             </div>
 
-            <!-- terms and conditions tickbox -->
-            <div class="text-nowrap">
-              <input type="checkbox" id="terms" name="terms" value="terms" ref="terms" class="me-2"/>
-              <label for="terms" class="text-wrap align-top">
-                By submitting this form, I/we confirm that the provided information is true and accurate to the best of my/our knowledge.
+            <div class="mt-5">
+              <label class="form-label" for="teamMembers">
+                Team Members
+                <span class="text-danger">*</span>
               </label>
-            </div>
-
-            <div class="nav-btn mt-5 d-flex align-items-center justify-content-center">
-              <button type="button" class="btn btn-outline-primary me-3" @click="save">Save</button>
-              <button type="button" class="btn btn-primary" @click="submit">Submit</button>
+              <div class="">
+                <div class="mb-2" v-for="(member, counter) in teamMembers" v-bind:key="counter">
+                  <label for="duration" class="me-2">{{ counter + 1 }}. Member - Name:</label>
+                  <input type="text" class="me-5 rounded" v-model.lazy="member.name" required />
+                  <label for="duration" class="me-2">Role:</label>
+                  <input type="text" class="me-2 rounded" v-model.lazy="member.role" required />
+                  <span class="delete-btn" @click="deleteMember(counter)">x</span>
+                </div>
+                <div class="w-50 d-flex justify-content-center">
+                  <i class="bi bi-plus-circle add-btn" @click="addMember"></i>
+                </div>
+              </div> -->
             </div>
           </div>
         </Transition>
       </section>
 
+      <section class="border-1 mb-5">
+        <div
+          class="mt-1 mb-3 py-2 px-3 secHeader d-flex align-items-center"
+          @click="show3 = !show3"
+        >
+          <i class="bi bi-caret-down-square-fill pe-2"></i>
+          <h5 class="mb-0 me-2">Part C: Objectives of the DLT Foundation</h5>
+          <span v-show="complete3 == true" class="text-success">✓</span>
+          <span v-show="complete3 != true" class="text-danger">*</span>
+        </div>
 
+        <Transition name="smooth" mode="out-in">
+          <div v-if="show3" class="form-content ms-3">
+            <LabelTextareaComponent label="Objectives of your DLT Foundation" 
+              footnote="Please provide a detailed description of the objectives of your DLT Foundation (max. 5000 characters)" 
+              type="text" v-model:field="objective" :required="true"/>
+          </div>
+        </Transition>
+      </section>
 
+      <section class="border-1 mb-5">
+        <div
+          class="mt-1 mb-3 py-2 px-3 secHeader d-flex align-items-center"
+          @click="show4 = !show4"
+        >
+          <i class="bi bi-caret-down-square-fill pe-2"></i>
+          <h5 class="mb-0 me-2">Part D: Specific Purpose</h5>
+          <span v-show="complete4 == true" class="text-success">✓</span>
+          <span v-show="complete4 != true" class="text-danger">*</span>
+        </div>
+
+        <Transition name="smooth" mode="out-in">
+          <div v-if="show4" class="form-content ms-3">
+            <LabelTextareaComponent label="Motivation of your DLT Foundation" 
+              footnote="If your DLT Foundation has been established for a specific purpose, please detail the purpose (max. 5000 characters)" 
+              type="text" v-model:field="motivation" :required="true"/>
+          </div>
+        </Transition>
+      </section>
+
+      <section class="border-1 mb-3">
+        <div
+          class="mt-1 mb-3 py-2 px-3 secHeader d-flex align-items-center"
+          @click="show5 = !show5"
+        >
+          <i class="bi bi-caret-down-square-fill pe-2"></i>
+          <h5 class="mb-0 me-2">Part E: Initial Assets</h5>
+          <span v-show="complete5 == true" class="text-success">✓</span>
+          <span v-show="complete5 != true" class="text-danger">*</span>
+        </div>
+
+        <Transition name="smooth" mode="out-in">
+          <div v-if="show5" class="form-content ms-3">
+            <LabelTextareaComponent label="Initial Assets of your DLT Foundation" 
+              footnote="Please provide a detailed description of the initial assets of your DLT Foundation (max. 5000 characters):" 
+              type="text" v-model:field="assets" :required="true"/>
+          </div>
+        </Transition>
+      </section>
+
+      <!-- terms and conditions tickbox -->
+      <div class="px-3 text-nowrap">
+        <input type="checkbox" id="terms" name="terms" value="terms" ref="terms" class="me-2"/>
+        <label for="terms" class="text-wrap align-top">
+          By submitting this form, I/we confirm that the provided information is true and accurate to the best of my/our knowledge.
+        </label>
+      </div>
+
+      <div class="nav-btn mt-5 d-flex align-items-center justify-content-center">
+        <button type="button" class="btn btn-outline-primary me-3" @click="save">Save</button>
+        <button type="button" class="btn btn-primary" @click="submit">Submit</button>
+      </div>
     </div>
   </div>
 </template>
@@ -250,7 +305,6 @@ export default {
       show3: true,
       show4: true,
       show5: true,
-      current: 1,
       name: '',
       twitter: '',
       website: '',
@@ -415,22 +469,7 @@ export default {
     },
     alert() {
       window.alert(this.name)
-    },
-    nextStep(curr: number) {
-      const mapping = {
-        1: this.complete1,
-        2: this.complete2,
-        3: this.complete3,
-        4: this.complete4,
-        5: this.complete5
-      }
-      if (mapping[curr as keyof typeof mapping]) {
-        this.current = curr + 1
-      } else {
-        window.alert('Please fill in all required fields')
-      }
-
-    } 
+    }
   },
   watch: {
     numFounders: function (val: string) {
@@ -473,12 +512,12 @@ export default {
 }
 
 .form-content {
-  max-height: 2000px;
+  max-height: 800px;
   overflow-y: hidden;
   overflow-x: hidden
 }
 .form-content2 {
-  max-height: 3000px;
+  max-height: 2000px;
   overflow-y: hidden;
   overflow-x: hidden
 }
@@ -494,11 +533,11 @@ export default {
 
 .smooth-enter-active,
 .smooth-leave-active {
-  transition: opacity 0.3s;
+  transition: max-height 0.3s;
 }
 .smooth-enter,
 .smooth-leave-to {
-  opacity: 0;
+  max-height: 0;
 }
 
 .form-controls {
@@ -524,9 +563,5 @@ export default {
   border-color: #86b7fe;
   outline: 0;
   box-shadow: 0 0 0 0.25rem rgba(13, 110, 253, 0.25);
-}
-
-.active-section {
-  text-decoration: underline;
 }
 </style>
