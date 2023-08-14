e850ed24c5ec05a7cae8857062bba84129f96215
Yiwei Gong <imwithye@gmail.com>
2023-08-14 10:43:52 +0800

    remove ID

--- src/views/accountSubViews/AccountApplicationsView.vue
+++ src/views/accountSubViews/AccountApplicationsView.vue
@@ -4,36 +4,36 @@
     <RouterLink class="btn btn-primary" to="/create-application">New Application</RouterLink>
   </div>
 
-  <table class="table table-bordered">
-    <thead class="table-dark">
-      <tr>
-        <th scope="col" style="width: 20%">ID</th>
-        <th scope="col" style="width: 40%">Project Name</th>
-        <th scope="col" style="width: 10%">Updated At</th>
-        <th scope="col" style="width: 10%">Created At</th>
-        <th scope="col" style="width: 8%">Status</th>
-        <th scope="col" style="width: 12%">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="application in applications" :key="application.ID">
-        <td style="text-transform: uppercase">{{ getSubmission(application).SubmissionID }}</td>
-        <td>{{ getSubmission(application).Content.Name }}</td>
-        <td>{{ formatDate(getSubmission(application).SubmissionAt) }}</td>
-        <td>{{ formatDate(getSubmission(application).SubmissionAt) }}</td>
-        <td>{{ 'Pending' }}</td>
-        <td>
-          <a
-            class="btn btn-sm btn-outline-primary mx-2"
-            type="button"
-            :href="'/projects/' + getSubmission(application).SubmissionID"
-            >View</a
-          >
-          <a class="btn btn-sm btn-outline-danger mx-2" type="button">Withdraw</a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="w-100" style="overflow-x: auto">
+    <table class="table table-bordered">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col" style="width: 40%">Project Name</th>
+          <th scope="col" style="width: 10%">Updated At</th>
+          <th scope="col" style="width: 10%">Created At</th>
+          <th scope="col" style="width: 8%">Status</th>
+          <th scope="col" style="width: 12%; min-width: 200px">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="application in applications" :key="application.ID">
+          <td>{{ getSubmission(application).Content.Name }}</td>
+          <td>{{ formatDate(getSubmission(application).SubmissionAt) }}</td>
+          <td>{{ formatDate(getSubmission(application).SubmissionAt) }}</td>
+          <td>{{ 'Pending' }}</td>
+          <td>
+            <a
+              class="btn btn-sm btn-outline-primary mx-2"
+              type="button"
+              :href="'/projects/' + application.ID"
+              >View</a
+            >
+            <a class="btn btn-sm btn-outline-danger mx-2" type="button">Withdraw</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
