473b1440a98ac97f781af8906aafb8304437dadc
Boon Chong Peh <pehboonchong@gmail.com>
2023-09-28 17:25:26 +0800

    added helm for deployment

--- src/httpclient.ts
+++ src/httpclient.ts
@@ -8,7 +8,8 @@ interface Response<T> {
 }
 
 class HttpClient {
-  public baseUrl: string = 'https://f9a2s4wxdj.execute-api.ap-southeast-1.amazonaws.com/staging'
+  // public baseUrl: string = 'https://f9a2s4wxdj.execute-api.ap-southeast-1.amazonaws.com/staging'
+  public baseUrl: string = 'https://prod-app-api.io'
   // public baseUrl: string = "http://localhost:3000"
 
   private _token: string = ''
