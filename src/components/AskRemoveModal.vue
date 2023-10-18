83b2f9f8b8c1231b1902d52953e1499ce7f1872e
Yiwei Gong <imwithye@gmail.com>
2023-10-18 22:33:09 +0800

    update remove modal

--- src/components/AskRemoveModal.vue
+++ src/components/AskRemoveModal.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="modal modal-lg fade"
+    class="modal fade"
     :class="{ show: visible, hidden: !visible }"
     tabindex="-1"
     style="display: block; background-color: rgba(0, 0, 0, 0.5)"
@@ -8,7 +8,7 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Tips</h5>
+          <h5 class="modal-title">Remove?</h5>
           <button
             type="button"
             class="btn-close"
@@ -18,7 +18,7 @@
           ></button>
         </div>
         <div class="modal-body">
-          <p>Are you sure to delete this person? Once deleted, it cannot be recovered.</p>
+          <p>Are you sure to remove this person's information?</p>
         </div>
         <div class="modal-footer">
           <button
@@ -29,9 +29,7 @@
           >
             Close
           </button>
-          <button type="button" class="btn btn-primary" @click="$emit('sure')">
-            Yes, I am sure
-          </button>
+          <button type="button" class="btn btn-danger" @click="$emit('ok')">Remove</button>
         </div>
       </div>
     </div>
