fad1a84e6fd8fdad3e2654eb2a9c60d8a93e8526
xiaolin820 <731199092@qq.com>
2023-10-20 00:37:40 +0800

    change the file

--- src/views/forms/RegistrationAgreementForm.vue
+++ src/views/forms/RegistrationAgreementForm.vue
@@ -2,7 +2,7 @@
   <div style="max-width: 960px">
     <SectionLayout title="Registration Agreement">
       <PDFComponent
-        src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210101201653/PDF.pdf"
+        src="https://github.com/insightic/webapp/blob/dev/src/assets/insightic.pdf"
         style="height: 480px"
       />
       <LabelInputComponent label="Title" type="text" v-model:field="title" />
