cabdeec61e039a20fdfaebbc31e171c22020bfa8
Yiwei Gong <imwithye@gmail.com>
2023-11-01 23:54:50 +0800

    better dashboard design

--- src/views/submissionSubViews/SubmissionDashboard.vue
+++ src/views/submissionSubViews/SubmissionDashboard.vue
@@ -10,13 +10,17 @@
       </div>
       <div class="col-2">
         <div class="h-100">
-          <div style="background-color: var(--bs-success); height: 50%" class="w-100">
-            <div>Highest</div>
-            <div>71</div>
+          <div style="position: relative; background-color: rgb(158, 248, 3); height: 50%" class="w-100 p-2">
+            <div class="small">Highest</div>
+            <div style="position: absolute; left: 50%; top: 50%; transform: translateX(-50%) translateY(-20%);">
+              <h4>71</h4>
+            </div>
           </div>
-          <div style="background-color: var(--bs-success); height: 50%" class="w-100">
-            <div>Lowest</div>
-            <div>40</div>
+          <div style="position: relative; background-color:chocolate; height: 50%" class="w-100 p-2">
+            <div class="small">Lowest</div>
+            <div style="position: absolute; left: 50%; top: 50%; transform: translateX(-50%) translateY(-20%);">
+              <h4>10</h4>
+            </div>
           </div>
         </div>
       </div>
@@ -33,18 +37,26 @@
         <table class="w-100 h-100 table table-bordered text-center" style="border-color: black">
           <thead>
             <tr>
-              <th style="border-left: 1px solid black;">Security Audit Status</th>
+              <th style="border-left: 1px solid black">Security Audit Status</th>
               <th>KYT Service Providers</th>
               <th>Security Auditors</th>
-              <th style="border-right: 1px solid black;">Incident Response Plan</th>
+              <th style="border-right: 1px solid black">Incident Response Plan</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td style="border-left: 1px solid black;">Pass</td>
-              <td>Internal Chainalysis Elliptic</td>
-              <td>Certik Hacken</td>
-              <td style="border-right: 1px solid black;">Ready</td>
+              <td style="border-left: 1px solid black">
+                <h5>Pass</h5>
+              </td>
+              <td>
+                <h5>Internal Chainalysis Elliptic</h5>
+              </td>
+              <td>
+                <h5>Certik Hacken</h5>
+              </td>
+              <td style="border-right: 1px solid black">
+                <h5>Ready</h5>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -55,56 +67,56 @@
         <table class="w-100 h-100 table table-bordered text-center" style="border-color: black">
           <thead>
             <tr>
-              <th style="border-left: 1px solid black;">Security Audits</th>
+              <th style="border-left: 1px solid black">Security Audits</th>
               <th>Number of Incidents</th>
               <th>Total Loss</th>
               <th>Critical Issues</th>
-              <th style="border-right: 1px solid black;">Non-Critical Issues</th>
+              <th style="border-right: 1px solid black">Non-Critical Issues</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td style="border-left: 1px solid black;">
-                <div>1</div>
-                <div>in 6 months</div>
+              <td style="border-left: 1px solid black">
+                <div class="fs-4">1</div>
+                <div class="small text-secondary">in 6 months</div>
               </td>
               <td>
-                <div>0</div>
-                <div>in 6 months</div>
+                <div class="fs-4">0</div>
+                <div class="small text-secondary">in 6 months</div>
               </td>
               <td>
-                <div>0</div>
-                <div>in 6 months</div>
+                <div class="fs-4">0</div>
+                <div class="small text-secondary">in 6 months</div>
               </td>
               <td>
-                <div>0</div>
-                <div>in 6 months</div>
+                <div class="fs-4">0</div>
+                <div class="small text-secondary">in 6 months</div>
               </td>
-              <td style="border-right: 1px solid black;">
-                <div>1</div>
-                <div>in 6 months</div>
+              <td style="border-right: 1px solid black">
+                <div class="fs-4">1</div>
+                <div class="small text-secondary">in 6 months</div>
               </td>
             </tr>
             <tr>
-              <td style="border-left: 1px solid black;">
-                <div>5</div>
-                <div>in 2 years</div>
+              <td style="border-left: 1px solid black">
+                <div class="fs-4">5</div>
+                <div class="small text-secondary">in 2 years</div>
               </td>
               <td>
-                <div>0</div>
-                <div>in 2 years</div>
+                <div class="fs-4">0</div>
+                <div class="small text-secondary">in 2 years</div>
               </td>
               <td>
-                <div>0</div>
-                <div>in 2 years</div>
+                <div class="fs-4">0</div>
+                <div class="small text-secondary">in 2 years</div>
               </td>
               <td>
-                <div>1</div>
-                <div>in 2 years</div>
+                <div class="fs-4">1</div>
+                <div class="small text-secondary">in 2 years</div>
               </td>
-              <td style="border-right: 1px solid black;">
-                <div>17</div>
-                <div>in 2 years</div>
+              <td style="border-right: 1px solid black">
+                <div class="fs-4">17</div>
+                <div class="small text-secondary">in 2 years</div>
               </td>
             </tr>
           </tbody>
@@ -123,18 +135,24 @@
         <table class="w-100 h-100 table table-bordered text-center" style="border-color: black">
           <thead>
             <tr>
-              <th style="border-left: 1px solid black;">Registration Status</th>
+              <th style="border-left: 1px solid black">Registration Status</th>
               <th>Age</th>
               <th>Auditor</th>
-              <th style="border-right: 1px solid black;">Audit Status</th>
+              <th style="border-right: 1px solid black">Audit Status</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td style="border-left: 1px solid black;">Active</td>
-              <td>3 Years<br />32 days</td>
-              <td>Ernst & Young - Middle East</td>
-              <td style="border-right: 1px solid black;">Pass</td>
+              <td style="border-left: 1px solid black">
+                <h5>Active</h5>
+              </td>
+              <td>
+                <h5>3 Years<br />32 days</h5>
+              </td>
+              <td><h5>Ernst & Young - Middle East</h5></td>
+              <td style="border-right: 1px solid black">
+                <h5>Pass</h5>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -145,41 +163,45 @@
         <table class="w-100 h-100 table table-bordered text-center" style="border-color: black">
           <thead>
             <tr>
-              <th style="border-left: 1px solid black;">Charter Score</th>
+              <th style="border-left: 1px solid black">Charter Score</th>
               <th>Issues</th>
               <th>Licensed Vendors</th>
-              <th style="border-right: 1px solid black;">Unlicensed Vendors</th>
+              <th style="border-right: 1px solid black">Unlicensed Vendors</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td style="border-left: 1px solid black;">95</td>
-              <td>
-                <div>0</div>
-                <div>in 6 months</div>
+              <td style="border-left: 1px solid black">
+                <h5>95</h5>
               </td>
               <td>
                 <div>0</div>
-                <div>in 6 months</div>
+                <div class="small text-secondary">in 6 months</div>
               </td>
-              <td style="border-right: 1px solid black;">
+              <td>
+                <div>0</div>
+                <div class="small text-secondary">in 6 months</div>
+              </td>
+              <td style="border-right: 1px solid black">
                 <div>1</div>
-                <div>in 6 months</div>
+                <div class="small text-secondary">in 6 months</div>
               </td>
             </tr>
             <tr>
-              <td style="border-left: 1px solid black;">Last update 1 June 2023</td>
+              <td style="border-left: 1px solid black">
+                <h5>Last update 1 June 2023</h5>
+              </td>
               <td>
                 <div>4</div>
-                <div>in 2 years</div>
+                <div class="small text-secondary">in 2 years</div>
               </td>
               <td>
                 <div>0</div>
-                <div>in 2 years</div>
+                <div class="small text-secondary">in 2 years</div>
               </td>
-              <td style="border-right: 1px solid black;">
+              <td style="border-right: 1px solid black">
                 <div>3</div>
-                <div>in 2 years</div>
+                <div class="small text-secondary">in 2 years</div>
               </td>
             </tr>
           </tbody>
@@ -210,7 +232,8 @@ export default {
 table {
   vertical-align: middle;
 }
-td, th {
+td,
+th {
   border-left: unset;
   border-right: unset;
 }
