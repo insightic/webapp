1542cd22a1e566733794ff43962edf960048a821
Yiwei Gong <imwithye@gmail.com>
2023-11-01 23:13:03 +0800

    add basic table view

--- src/views/submissionSubViews/SubmissionDashboard.vue
+++ src/views/submissionSubViews/SubmissionDashboard.vue
@@ -1,13 +1,197 @@
 <template>
-  <div class="row">
-    <div class="col-3">
-      <ScoreBoard />
+  <div class="mb-4">
+    <h3>Scores</h3>
+    <div class="row">
+      <div class="col-3">
+        <ScoreBoard />
+      </div>
+      <div class="col-7">
+        <LineChart />
+      </div>
+      <div class="col-2">
+        <div class="h-100">
+          <div style="background-color: var(--bs-success); height: 50%" class="w-100">
+            <div>Highest</div>
+            <div>71</div>
+          </div>
+          <div style="background-color: var(--bs-success); height: 50%" class="w-100">
+            <div>Lowest</div>
+            <div>40</div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="col-7">
-      <LineChart />
-    </div>
-    <div class="col-1"></div>
   </div>
+
+  <div class="mb-4">
+    <h3>Security Assurance</h3>
+    <div class="row mb-3">
+      <div class="col-3">
+        <ScoreBoard />
+      </div>
+      <div class="col-9">
+        <table class="w-100 h-100 table text-center" style="border-color: black">
+          <thead>
+            <tr>
+              <th>Security Audit Status</th>
+              <th>KYT Service Providers</th>
+              <th>Security Auditors</th>
+              <th>Incident Response Plan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Pass</td>
+              <td>Internal Chainalysis Elliptic</td>
+              <td>Certik Hacken</td>
+              <td>Ready</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <table class="w-100 h-100 table text-center" style="border-color: black">
+          <thead>
+            <tr>
+              <th>Security Audits</th>
+              <th>Number of Incidents</th>
+              <th>Total Loss</th>
+              <th>Critical Issues</th>
+              <th>Non-Critical Issues</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div>1</div>
+                <div>in 6 months</div>
+              </td>
+              <td>
+                <div>0</div>
+                <div>in 6 months</div>
+              </td>
+              <td>
+                <div>0</div>
+                <div>in 6 months</div>
+              </td>
+              <td>
+                <div>0</div>
+                <div>in 6 months</div>
+              </td>
+              <td>
+                <div>1</div>
+                <div>in 6 months</div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div>5</div>
+                <div>in 2 years</div>
+              </td>
+              <td>
+                <div>0</div>
+                <div>in 2 years</div>
+              </td>
+              <td>
+                <div>0</div>
+                <div>in 2 years</div>
+              </td>
+              <td>
+                <div>1</div>
+                <div>in 2 years</div>
+              </td>
+              <td>
+                <div>17</div>
+                <div>in 2 years</div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+  <div class="mb-4">
+    <h3>Regulatory Compliance</h3>
+    <div class="row mb-4">
+      <div class="col-3">
+        <ScoreBoard />
+      </div>
+      <div class="col-9">
+        <table class="w-100 h-100 table text-center" style="border-color: black">
+          <thead>
+            <tr>
+              <th>Registration Status</th>
+              <th>Age</th>
+              <th>Auditor</th>
+              <th>Audit Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Active</td>
+              <td>3 Years<br />32 days</td>
+              <td>Ernst & Young - Middle East</td>
+              <td>Pass</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <table class="w-100 h-100 table text-center" style="border-color: black">
+          <thead>
+            <tr>
+              <th>Charter Score</th>
+              <th>Issues</th>
+              <th>Licensed Vendors</th>
+              <th>Unlicensed Vendors</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                95
+              </td>
+              <td>
+                <div>0</div>
+                <div>in 6 months</div>
+              </td>
+              <td>
+                <div>0</div>
+                <div>in 6 months</div>
+              </td>
+              <td>
+                <div>1</div>
+                <div>in 6 months</div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Last update 1 June 2023
+              </td>
+              <td>
+                <div>4</div>
+                <div>in 2 years</div>
+              </td>
+              <td>
+                <div>0</div>
+                <div>in 2 years</div>
+              </td>
+              <td>
+                <div>3</div>
+                <div>in 2 years</div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
   <!-- <div class="row">
     <DashboardSectionLayout bg-color="white">
       <ItemDetails></ItemDetails>
