b66dd6ae1c3e8f90a6b84b6dfc49b1644d94e0b7
Yiwei Gong <imwithye@gmail.com>
2024-01-07 21:54:41 +0800

    add social media chart

---
+++ src/components/dashboard/TwitterFollowers.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h3 class="card-title">{{ title }}</h3>
+
+      <VueApexCharts height="196px" type="area" :series="data" :options="chartOptions" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, type PropType } from 'vue'
+import VueApexCharts from 'vue3-apexcharts'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  labels: {
+    type: Array as PropType<string[]>,
+    required: true
+  },
+  data: {
+    type: Array as PropType<Array<{ name: string; data: Array<number> }>>,
+    required: true
+  }
+})
+
+const chartOptions = ref({
+  chart: {
+    toolbar: {
+      show: false
+    },
+    animations: {
+      enabled: false
+    },
+    stacked: true
+  },
+  plotOptions: {
+    bar: {
+      columnWidth: '50%'
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  fill: {
+    opacity: 1
+  },
+  grid: {
+    padding: {
+      top: -20,
+      right: 0,
+      left: -4,
+      bottom: -4
+    },
+    strokeDashArray: 4,
+    xaxis: {
+      lines: {
+        show: true
+      }
+    }
+  },
+  stroke: {
+    width: 2,
+    lineCap: 'round',
+    curve: 'smooth'
+  },
+  xaxis: {
+    labels: {
+      padding: 0
+    },
+    tooltip: {
+      enabled: false
+    },
+    axisBorder: {
+      show: false
+    },
+    type: 'datetime'
+  },
+  yaxis: {
+    labels: {
+      padding: 4
+    }
+  },
+  labels: props.labels,
+  colors: ['#206bc4', '#206bc4aa', '#2fb344aa'],
+  legend: {
+    show: false
+  }
+})
+</script>
--- src/views/applicationSubViews/DashboardView.vue
+++ src/views/applicationSubViews/DashboardView.vue
@@ -7,7 +7,11 @@
           <div class="card-body d-flex align-items-center">
             <div class="row align-items-center w-100 h-100">
               <div class="col-3">
-                <img :src="submission?.Content?.LogoURL" class="rounded-circle" style="width: 60%" />
+                <img
+                  :src="submission?.Content?.LogoURL"
+                  class="rounded-circle"
+                  style="width: 60%"
+                />
               </div>
               <div class="col">
                 <div class="card-title mb-0">
@@ -32,7 +36,7 @@
 
   <!-- General Information -->
   <div>
-    <div class="d-flex align-items-center" style="cursor: pointer;">
+    <div class="d-flex align-items-center" style="cursor: pointer">
       <h2>General Information</h2>
       <div class="ms-auto">Last Update {{ formatDateTime(new Date()) }}</div>
     </div>
@@ -70,8 +74,11 @@
         </div>
       </div>
       <div class="col-lg-6">
-        <TableComponent title="test" :columns="['Name', 'Information']"
-          :data="[['Title', 'Insightic - Web3 Compliance Made Easy']]" />
+        <TableComponent
+          title="test"
+          :columns="['Name', 'Information']"
+          :data="[['Title', 'Insightic - Web3 Compliance Made Easy']]"
+        />
       </div>
       <div class="col-lg-6">
         <div class="card">
@@ -149,132 +156,211 @@
   </div>
 
   <!-- Stable Coin -->
-  <div v-if="transcations || totalSupply || topKHolder || tokenPrice || mintBurn || holdAgeFreqPortfilio">
-    <div class="d-flex align-items-center" style="cursor: pointer;">
+  <div
+    v-if="
+      transcations || totalSupply || topKHolder || tokenPrice || mintBurn || holdAgeFreqPortfilio
+    "
+  >
+    <div class="d-flex align-items-center" style="cursor: pointer">
       <h2>Stable Coin</h2>
       <div class="ms-auto">Last Update {{ formatDateTime(new Date()) }}</div>
     </div>
 
     <div class="row row-deck row-cards mb-3">
       <div v-if="transcations" class="col-lg-12">
-        <TableComponent title="Average Rate" :columns="transcations.avg_rate.columns" :data="transcations.avg_rate.data"
-          :update-at="updateAt" />
+        <TableComponent
+          title="Average Rate"
+          :columns="transcations.avg_rate.columns"
+          :data="transcations.avg_rate.data"
+          :update-at="updateAt"
+        />
       </div>
       <div v-if="transcations" class="col-lg-6">
-        <ChartComponent type="bar" title="Daily Frequency"
-          :labels="[transcations.daily_frequency.data.map((d: any) => `${d[0]}`)]" :data="[
+        <ChartComponent
+          type="bar"
+          title="Daily Frequency"
+          :labels="[transcations.daily_frequency.data.map((d: any) => `${d[0]}`)]"
+          :data="[
             {
               name: 'Daily Frequency',
               data: transcations.daily_frequency.data.map((d: any) => Number(d[1]))
             }
-          ]" :update-at="updateAt" />
+          ]"
+          :update-at="updateAt"
+        />
       </div>
       <div v-if="transcations" class="col-lg-6">
-        <ChartComponent type="bar" title="Hourly Frequency"
-          :labels="[transcations.hourly_frequency.data.map((d: any) => `${d[0]}:00`)]" :data="[
+        <ChartComponent
+          type="bar"
+          title="Hourly Frequency"
+          :labels="[transcations.hourly_frequency.data.map((d: any) => `${d[0]}:00`)]"
+          :data="[
             {
               name: 'Hourly Frequency',
               data: transcations.hourly_frequency.data.map((d: any) => Number(d[1]))
             }
-          ]" :update-at="updateAt" />
+          ]"
+          :update-at="updateAt"
+        />
       </div>
       <div v-if="transcations" class="col-lg-6">
-        <ChartComponent title="Daily Bought/Sold Volume" :labels="[
-          transcations.daily_bought_vol.data.map((d: any) => `${d[0]}`),
-          transcations.daily_sold_vol.data.map((d: any) => `${d[0]}`)
-        ]" :data="[
-  {
-    name: 'Daily Bought Volume',
-    data: transcations.daily_bought_vol.data.map((d: any) => Number(d[1]))
-  },
-  {
-    name: 'Daily Sold Volume',
-    data: transcations.daily_sold_vol.data.map((d: any) => Number(d[1]))
-  }
-]" :update-at="updateAt" />
+        <ChartComponent
+          title="Daily Bought/Sold Volume"
+          :labels="[
+            transcations.daily_bought_vol.data.map((d: any) => `${d[0]}`),
+            transcations.daily_sold_vol.data.map((d: any) => `${d[0]}`)
+          ]"
+          :data="[
+            {
+              name: 'Daily Bought Volume',
+              data: transcations.daily_bought_vol.data.map((d: any) => Number(d[1]))
+            },
+            {
+              name: 'Daily Sold Volume',
+              data: transcations.daily_sold_vol.data.map((d: any) => Number(d[1]))
+            }
+          ]"
+          :update-at="updateAt"
+        />
       </div>
       <div v-if="transcations" class="col-lg-6">
-        <ChartComponent title="Hourly Bought/Sold Volume" :labels="[
-          transcations.hourly_bought_vol.data.map((d: any) => `${d[0]}:00`),
-          transcations.hourly_sold_vol.data.map((d: any) => `${d[0]}:00`)
-        ]" :data="[
-  {
-    name: 'Hourly Bought Volume',
-    data: transcations.hourly_bought_vol.data.map((d: any) => Number(d[1]))
-  },
-  {
-    name: 'Hourly Sold Volume',
-    data: transcations.hourly_sold_vol.data.map((d: any) => Number(d[1]))
-  }
-]" :update-at="updateAt" />
+        <ChartComponent
+          title="Hourly Bought/Sold Volume"
+          :labels="[
+            transcations.hourly_bought_vol.data.map((d: any) => `${d[0]}:00`),
+            transcations.hourly_sold_vol.data.map((d: any) => `${d[0]}:00`)
+          ]"
+          :data="[
+            {
+              name: 'Hourly Bought Volume',
+              data: transcations.hourly_bought_vol.data.map((d: any) => Number(d[1]))
+            },
+            {
+              name: 'Hourly Sold Volume',
+              data: transcations.hourly_sold_vol.data.map((d: any) => Number(d[1]))
+            }
+          ]"
+          :update-at="updateAt"
+        />
       </div>
     </div>
 
     <div class="row row-deck row-cards mb-3">
       <div v-if="totalSupply" class="col-lg-12">
-        <ChartComponent title="Token Supply Over Time" :labels="[totalSupply.data.map((d: any) => String(d[0]))]" :data="[
-          { name: 'Token Supply Over Time', data: totalSupply.data.map((d: any) => Number(d[1])) }
-        ]" :update-at="updateAt" />
+        <ChartComponent
+          title="Token Supply Over Time"
+          :labels="[totalSupply.data.map((d: any) => String(d[0]))]"
+          :data="[
+            { name: 'Token Supply Over Time', data: totalSupply.data.map((d: any) => Number(d[1])) }
+          ]"
+          :update-at="updateAt"
+        />
       </div>
       <div v-if="tokenPrice" class="col-lg-6">
-        <ChartComponent title="Token Price in USD over time" :labels="[tokenPrice.data.map((d: any) => String(d[0]))]"
+        <ChartComponent
+          title="Token Price in USD over time"
+          :labels="[tokenPrice.data.map((d: any) => String(d[0]))]"
           :data="[
             {
               name: 'Token Price in USD over time',
               data: tokenPrice.data.map((d: any) => Number(d[1]))
             }
-          ]" :update-at="updateAt" />
+          ]"
+          :update-at="updateAt"
+        />
       </div>
       <div v-if="tokenPrice" class="col-lg-6">
-        <ChartComponent title="Token Price in ETH over time" :labels="[tokenPrice.data.map((d: any) => String(d[0]))]"
+        <ChartComponent
+          title="Token Price in ETH over time"
+          :labels="[tokenPrice.data.map((d: any) => String(d[0]))]"
           :data="[
             {
               name: 'Token Price in ETH over time',
               data: tokenPrice.data.map((d: any) => Number(d[3]))
             }
-          ]" :update-at="updateAt" />
+          ]"
+          :update-at="updateAt"
+        />
       </div>
       <div v-if="mintBurn" class="col-lg-6">
         <TableComponent title="Mint/Burn" :columns="mintBurn.columns" :data="mintBurn.data" />
       </div>
       <div v-if="topKHolder" class="col-lg-6">
-        <PieChartComponent title="Top 10 holders" :labels="topKHolder.data.map((d: any) => String(d[0]).substring(0, 10))"
-          :data="topKHolder.data.map((d: any) => d[3])" :update-at="updateAt" />
+        <PieChartComponent
+          title="Top 10 holders"
+          :labels="topKHolder.data.map((d: any) => String(d[0]).substring(0, 10))"
+          :data="topKHolder.data.map((d: any) => d[3])"
+          :update-at="updateAt"
+        />
       </div>
       <div v-if="holdAgeFreqPortfilio" class="col-lg-6">
-        <PieChartComponent title="Age Stats" :labels="holdAgeFreqPortfilio['age'].data.map((d: any) => String(d[1]))"
-          :data="holdAgeFreqPortfilio['age'].data.map((d: any) => Number(d[2]))" :update-at="updateAt" />
+        <PieChartComponent
+          title="Age Stats"
+          :labels="holdAgeFreqPortfilio['age'].data.map((d: any) => String(d[1]))"
+          :data="holdAgeFreqPortfilio['age'].data.map((d: any) => Number(d[2]))"
+          :update-at="updateAt"
+        />
       </div>
       <div v-if="holdAgeFreqPortfilio" class="col-lg-6">
-        <PieChartComponent title="Freq Stats" :labels="holdAgeFreqPortfilio['freq'].data.map((d: any) => String(d[1]))"
-          :data="holdAgeFreqPortfilio['freq'].data.map((d: any) => Number(d[2]))" :update-at="updateAt" />
+        <PieChartComponent
+          title="Freq Stats"
+          :labels="holdAgeFreqPortfilio['freq'].data.map((d: any) => String(d[1]))"
+          :data="holdAgeFreqPortfilio['freq'].data.map((d: any) => Number(d[2]))"
+          :update-at="updateAt"
+        />
       </div>
       <div v-if="holdAgeFreqPortfilio" class="col-lg-6">
-        <PieChartComponent title="Portfolio Stats"
+        <PieChartComponent
+          title="Portfolio Stats"
           :labels="holdAgeFreqPortfilio['portfolio'].data.map((d: any) => String(d[1]))"
-          :data="holdAgeFreqPortfilio['portfolio'].data.map((d: any) => Number(d[2]))" :update-at="updateAt" />
+          :data="holdAgeFreqPortfilio['portfolio'].data.map((d: any) => Number(d[2]))"
+          :update-at="updateAt"
+        />
       </div>
     </div>
   </div>
 
   <!-- Social Media -->
   <div>
-    <div class="d-flex align-items-center" style="cursor: pointer;">
+    <div class="d-flex align-items-center" style="cursor: pointer">
       <h2>Social Media</h2>
       <div class="ms-auto">Last Update {{ formatDateTime(new Date()) }}</div>
+    </div>
+
+    <div class="row row-deck row-cards mb-3">
+      <div class="col-lg-6">
+        <TwitterFollowers
+          title="Reddit Subscribers"
+          :labels="followers('Reddit Subscribers').label"
+          :data="followers('Reddit Subscribers').data"
+        />
+      </div>
+      <div class="col-lg-6">
+        <TwitterFollowers
+          title="Twitter Followers"
+          :labels="followers('Twitter Followers').label"
+          :data="followers('Twitter Followers').data"
+        />
+      </div>
     </div>
   </div>
 
   <!-- Security Assurance -->
   <div>
-    <div class="d-flex align-items-center" style="cursor: pointer;">
+    <div class="d-flex align-items-center" style="cursor: pointer">
       <h2>Security Assurance</h2>
       <div class="ms-auto">Last Update {{ formatDateTime(new Date()) }}</div>
     </div>
 
     <div class="row row-deck row-cards mb-3">
       <div class="col-lg-4">
-        <ScoreBoard title="Security Assurance" grade="A-" :NowData="'71'" :PreviousData="'70'" :ChangeData="'+1'" />
+        <ScoreBoard
+          title="Security Assurance"
+          grade="A-"
+          :NowData="'71'"
+          :PreviousData="'70'"
+          :ChangeData="'+1'"
+        />
       </div>
       <div class="col-lg-8">
         <div class="card">
@@ -299,11 +385,15 @@
                       {{ submission.Content['Risk Management']['SecurityAuditor'] }}
                     </div>
 
-                    <div v-if="submission.Content['Risk Management']['SecurityAuditor'] &&
-                      securityAuditorInfo
-                      " class="ms-2">
+                    <div
+                      v-if="
+                        submission.Content['Risk Management']['SecurityAuditor'] &&
+                        securityAuditorInfo
+                      "
+                      class="ms-2"
+                    >
                       <VDropdown>
-                        <button class="btn btn-sm btn-primary">Know More</button>
+                        <button class="btn btn-sm btn-outline-primary rounded">?</button>
 
                         <template #popper>
                           <div class="p-2" style="width: 480px">
@@ -311,14 +401,17 @@
                               <tr>
                                 <td class="text-secondary" style="width: 160px">Tier:</td>
                                 <td>
-                                  <span class="badge rounded-pill bg-success me-2 text-white">Tier {{
-                                    securityAuditorInfo['Tier'] }}</span>
+                                  <span class="badge rounded-pill bg-success me-2 text-white"
+                                    >Tier {{ securityAuditorInfo['Tier'] }}</span
+                                  >
                                 </td>
                               </tr>
                               <tr>
                                 <td class="text-secondary">Status:</td>
                                 <td>
-                                  <span class="badge rounded-pill bg-success me-2 text-white">active</span>
+                                  <span class="badge rounded-pill bg-success me-2 text-white"
+                                    >active</span
+                                  >
                                 </td>
                               </tr>
                               <tr>
@@ -487,14 +580,20 @@
 
   <!-- Regulatory Compliance -->
   <div>
-    <div class="d-flex align-items-center" style="cursor: pointer;">
+    <div class="d-flex align-items-center" style="cursor: pointer">
       <h2>Regulatory Compliance</h2>
       <div class="ms-auto">Last Update {{ formatDateTime(new Date()) }}</div>
     </div>
 
     <div class="row row-deck row-cards mb-3">
       <div class="col-lg-4">
-        <ScoreBoard title="Regulatory Compliance" grade="C+" :NowData="'71'" :PreviousData="'70'" :ChangeData="'+1'" />
+        <ScoreBoard
+          title="Regulatory Compliance"
+          grade="C+"
+          :NowData="'71'"
+          :PreviousData="'70'"
+          :ChangeData="'+1'"
+        />
       </div>
       <div class="col-lg-8">
         <div class="card">
@@ -619,10 +718,13 @@ import ScoreBoard from '@/components/dashboard/ScoreBoardComponent.vue'
 import TableComponent from '@/components/dashboard/TableComponent.vue'
 import ChartComponent from '@/components/dashboard/ChartComponent.vue'
 import PieChartComponent from '@/components/dashboard/PieChartComponent.vue'
+import TwitterFollowers from '@/components/dashboard/TwitterFollowers.vue'
 import { getJobResults } from '@/api'
 import Papa from 'papaparse'
 import auditors from '@/assets/auditors.csv?raw'
 import { formatDateTime } from '@/helpers'
+import SampleJSON from '@/views/applicationSubViews/sample_twitter.json'
+import moment from 'moment'
 
 export default {
   components: {
@@ -631,7 +733,8 @@ export default {
     ScoreBoard,
     TableComponent,
     ChartComponent,
-    PieChartComponent
+    PieChartComponent,
+    TwitterFollowers
   },
   props: ['application', 'submission'],
   async created() {
@@ -668,7 +771,19 @@ export default {
     }
   },
   methods: {
-    formatDateTime
+    formatDateTime,
+    followers(name: string) {
+      const sample = SampleJSON.followers_data.filter((f) => f.name == name)[0].data
+      return {
+        label: sample.map((s) => moment(s[0]).format('YYYY-MM-DD')),
+        data: [
+          {
+            name: 'Twitter Followers',
+            data: sample.map((s) => s[1])
+          }
+        ]
+      }
+    }
   },
   computed: {
     securityAuditorInfo() {
@@ -693,7 +808,7 @@ th {
   border-right: unset;
 }
 
-.auditor-info-table>tr>td {
+.auditor-info-table > tr > td {
   vertical-align: top;
 }
 </style>
