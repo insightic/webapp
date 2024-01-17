6196d69d108e322f2f3306cae4d497a13376ef7a
Yiwei Gong <imwithye@gmail.com>
2024-01-17 10:17:41 +0800

    reinstall all

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 : "${MODE:=production}"
 echo "App is running $MODE"
-cd /app && npm i && npm run build-only -- --mode $MODE
+cd /app && rm -rf node_modules && rm package-lock.json && npm i && npm run build-only -- --mode $MODE
 rm -rf /usr/share/nginx/html && mkdir -p /usr/share/nginx/html
 cp -r /app/dist/* /usr/share/nginx/html
 nginx -g "daemon off;"
