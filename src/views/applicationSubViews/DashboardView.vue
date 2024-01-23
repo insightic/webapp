3c47b565031b7fc5716fad05c6d63c73a1f81a04
Yiwei Gong <imwithye@gmail.com>
2024-01-23 10:51:59 +0800

    add linkedin

--- src/views/applicationSubViews/DashboardView.vue
+++ src/views/applicationSubViews/DashboardView.vue
@@ -24,35 +24,33 @@
                 <div class="text-secondary mb-2" style="font-size: 0.6rem">OFFICIAL LINKS</div>
                 <div class="d-flex mb-2">
                   <div class="me-2 badge bg-secondary">
-                    <a
-                      :href="submission?.Content?.Website"
-                      style="color: white; text-decoration: none; font-size: 0.6rem"
-                    >
+                    <a :href="submission?.Content?.Website"
+                      style="color: white; text-decoration: none; font-size: 0.6rem">
                       Whitepaper
                     </a>
                   </div>
                   <div class="me-2 badge bg-secondary">
-                    <a
-                      :href="submission?.Content?.Website"
-                      style="color: white; text-decoration: none; font-size: 0.6rem"
-                    >
+                    <a :href="submission?.Content?.Website"
+                      style="color: white; text-decoration: none; font-size: 0.6rem">
                       Contract
                     </a>
                   </div>
                 </div>
                 <div class="text-secondary mb-2" style="font-size: 0.6rem">SOCIAL MEDIAS</div>
                 <div class="d-flex mb-2">
-                  <a
-                    v-if="submission?.Content.Overview?.TwitterHandle"
-                    :href="'https://twitter.com/' + submission?.Content.Overview?.TwitterHandle"
-                    class="me-2 text-white"
-                    target="_blank"
-                  >
+                  <a v-if="submission?.Content?.Overview?.TwitterHandle"
+                    :href="'https://twitter.com/' + submission?.Content.Overview?.TwitterHandle" class="me-2 text-white"
+                    target="_blank">
                     <IconBrandX :size="24" />
                   </a>
 
+                  <a v-if="submission?.Content?.Overview?.LinkedinURL" :href="submission?.Content?.Overview?.LinkedinURL"
+                    class="me-2 text-white">
+                    <IconBrandLinkedin :size="24" />
+                  </a>
+
                   <IconBrandGithub class="me-2" :size="24" />
-                  <IconBrandLinkedin :size="24" />
+
                 </div>
               </div>
             </div>
@@ -212,14 +210,8 @@
 
     <div class="row row-deck row-cards mb-3">
       <div class="col-lg-4">
-        <ScoreBoard
-          title="Security Assurance"
-          grade="B+"
-          :NowData="securityReportInfo.final_score"
-          :PreviousData="'70'"
-          :ChangeData="'+1'"
-          :details="securityReportInfo.report_summary"
-        />
+        <ScoreBoard title="Security Assurance" grade="B+" :NowData="securityReportInfo.final_score" :PreviousData="'70'"
+          :ChangeData="'+1'" :details="securityReportInfo.report_summary" />
       </div>
       <div class="col-lg-8">
         <div class="card">
@@ -244,13 +236,9 @@
                       {{ submission.Content['Risk Management']['SecurityAuditor'] }}
                     </div>
 
-                    <div
-                      v-if="
-                        submission.Content['Risk Management']['SecurityAuditor'] &&
-                        securityAuditorInfo
-                      "
-                      class="ms-2"
-                    >
+                    <div v-if="submission.Content['Risk Management']['SecurityAuditor'] &&
+                      securityAuditorInfo
+                      " class="ms-2">
                       <VDropdown>
                         <button class="btn btn-sm btn-outline-primary rounded">?</button>
 
@@ -260,17 +248,14 @@
                               <tr>
                                 <td class="text-secondary" style="width: 160px">Tier:</td>
                                 <td>
-                                  <span class="badge rounded-pill bg-success me-2 text-white"
-                                    >Tier {{ securityAuditorInfo['Tier'] }}</span
-                                  >
+                                  <span class="badge rounded-pill bg-success me-2 text-white">Tier {{
+                                    securityAuditorInfo['Tier'] }}</span>
                                 </td>
                               </tr>
                               <tr>
                                 <td class="text-secondary">Status:</td>
                                 <td>
-                                  <span class="badge rounded-pill bg-success me-2 text-white"
-                                    >active</span
-                                  >
+                                  <span class="badge rounded-pill bg-success me-2 text-white">active</span>
                                 </td>
                               </tr>
                               <tr>
@@ -446,13 +431,7 @@
 
     <div class="row row-deck row-cards mb-3">
       <div class="col-lg-4">
-        <ScoreBoard
-          title="Regulatory Compliance"
-          grade="C+"
-          :NowData="'71'"
-          :PreviousData="'70'"
-          :ChangeData="'+1'"
-        />
+        <ScoreBoard title="Regulatory Compliance" grade="C+" :NowData="'71'" :PreviousData="'70'" :ChangeData="'+1'" />
       </div>
       <div class="col-lg-8">
         <div class="card">
@@ -633,7 +612,7 @@ th {
   border-right: unset;
 }
 
-.auditor-info-table > tr > td {
+.auditor-info-table>tr>td {
   vertical-align: top;
 }
 </style>
